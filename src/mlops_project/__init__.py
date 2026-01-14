07a4977b83e23dda1f45362c59d06c2624da3dc5
siddhesh.padwal <siddhesh.padwal10@gmail.com>
2026-01-13 22:49:44 -0600

    Fix package init and logger module; ensure imports work

---
+++ src/mlops_project/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for the ``mlops_project`` project.
+
+Do not import subpackage symbols at module import time to avoid
+import-time cycles when importing submodules such as
+``mlops_project.core.logger``.
+"""
+
+__all__ = []
---
+++ src/mlops_project/core/__init__.py
@@ -0,0 +1,1 @@
+"""Package initializer for mlops_project.core."""
